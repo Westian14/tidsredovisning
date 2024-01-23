2900353b1f9c346b07d7e753391dd05af433dc13
unknown <60915@gymnasium.ax>
2024-01-23 11:43:24 +0200

    Hämta poster (sida) och tester funkar

--- src/Tasks.php
+++ src/Tasks.php
@@ -54,8 +54,60 @@ function tasks(Route $route, array $postData): Response {
  * @param string $sida
  * @return Response
  */
-function hamtaSida(string $sida): Response {
-    
+function hamtaSida(string $sida, int $posterPerSida=10): Response {
+
+    //Kontrollera indata
+    $sidnummer=filter_var($sida, FILTER_VALIDATE_INT);
+    if($sidnummer===false || $sidnummer<1) {
+        $retur=new stdClass();
+        $retur->error=["Bad request", "Felaktigt angivet sidnummer"];
+        return new Response($retur, 400);
+    }
+
+    //Koppla databas
+    $db=connectDb();
+
+    //Hämta poster
+    $stmt=$db->query("SELECT COUNT(*) FROM uppgifter");
+    $antalPoster=$stmt->fetchColumn();
+    if(!$antalPoster) {
+        $retur=new stdClass();
+        $retur->error=["Inga poster kunde hittas"];
+        return new Response($retur, 400);
+    }
+
+    $antalSidor=ceil($antalPoster/$posterPerSida);
+    if($sidnummer>$antalSidor) {
+        $retur=new stdClass();
+        $retur->error=["Bad request", "Felaktigt sidnummer", "Det finns bara $antalSidor"];
+        return new Response($retur, 400);
+    }
+
+    $forstaPost=($sidnummer-1)*$posterPerSida;
+
+    $stmt=$db->query("SELECT u.ID, Datum, Tid, AktivitetID, Namn, Beskrivning "
+        . "FROM uppgifter u INNER JOIN aktiviteter a ON AktivitetID=a.ID "
+        . "ORDER BY Datum "
+        . "LIMIT $forstaPost, $posterPerSida");
+    $result=$stmt->fetchAll();
+    $uppgifter=[];
+    foreach($result as $row) {
+        $rad=new stdClass();
+        $rad->id=$row["ID"];
+        $rad->activityId=$row["AktivitetID"];   
+        $rad->date=$row["Datum"];
+        $tid=new DateTime($row["Tid"]);
+        $rad->time=$tid->format("H:i");
+        $rad->activity=$row["Namn"];
+        $rad->date=$row["Beskrivning"];
+        $uppgifter[]=$rad;
+    }
+
+    //Returnera svar
+    $retur=new stdClass();
+    $retur->pages=$antalSidor;
+    $retur->tasks=$uppgifter;
+    return new Response($retur);
 }
 
 /**
--- test/TestTasks.php
+++ test/TestTasks.php
@@ -23,10 +23,64 @@ function allaTaskTester(): string {
  * @return string html-sträng med alla resultat för testerna 
  */
 function test_HamtaUppgifterSida(): string {
-    $retur = "<h2>test_HamtaUppgifterSida</h2>";
+    $retur="<h2>test_HamtaUppgfiterSida</h2>";
     try {
-        $retur .= "<p class='error'>Inga tester implementerade</p>";
-    } catch (Exception $ex) {
+    //Misslyckas med att hämta sida -1
+    $svar=hamtaSida("-1");
+    if($svar->getStatus()===400) {
+        $retur .="<p class='ok'>Misslyckades med att hämta sida -1, som förväntat</p>";
+    }
+    else {
+        $retur .="<p class='error'>Misslyckades med att hämta sida -1<br>"
+        . $svar->getStatus() . " returnerades istället</p>";
+    }
+
+    //Misslyckas med att hämta sida 0
+    $svar=hamtaSida("0");
+    if($svar->getStatus()===400) {
+        $retur .="<p class='ok'>Misslyckades med att hämta sida 0, som förväntat</p>";
+    }
+    else {
+        $retur .="<p class='error'>Misslyckades med att hämta sida 0<br>"
+        . $svar->getStatus() . " returnerades istället</p>";
+    }
+
+    //Misslyckas med att hämta sida sju
+    $svar=hamtaSida("sju");
+    if($svar->getStatus()===400) {
+        $retur .="<p class='ok'>Misslyckades med att hämta sida sju, som förväntat</p>";
+    }
+    else {
+        $retur .="<p class='error'>Misslyckades med att hämta sida <i>sju<i><br>"
+        . $svar->getStatus() . " returnerades istället</p>";
+    }
+
+    //Lyckas med att hämta sida 1
+    $svar=hamtaSida("1",2);
+    if($svar->getStatus()===200) {
+        $retur .="<p class='ok'>Lyckades med att hämta sida 1</p>";
+        $sista=$svar->getContent()->pages;
+    }
+    else {
+        $retur .="<p class='error'>Misslyckades med att hämta sida 1<br>"
+        . $svar->getStatus() . " returnerades istället för förväntat 200</p>";
+    }
+
+    //Misslyckas med att hämta sida > antal sidor
+    if(isset($sista)) {
+        $sista++;
+        $svar=hamtaSida("$sista",2);
+        if($svar->getStatus()===400) {
+            $retur .="<p class='ok'>Misslyckades med att hämta sida > antal sidor</p>";
+        }
+        else {
+            $retur .="<p class='error'>Misslyckat test att hämta sida > antal sidor<br>"
+            . $svar->getStatus() . " returnerades istället för förväntat 200</p>";
+        }
+    }
+
+    } 
+    catch (Exception $ex) {
         $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
     }
 
